8e0c87dbbfbb430a9c24108dc1a5afe2aee950b6
Agnes Koinange <koinangeagness@gmail.com>
2023-06-28 11:28:55 +0300

    ft: style sign up and sign in page

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,21 +1,113 @@
-import React from 'react';
-import { Button, Checkbox, FormControlLabel, TextField } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import Web3 from 'web3';
+import { TextField, Button, Checkbox, FormControlLabel, Typography, Container, Grid } from '@mui/material';
+import { styled } from '@mui/system';
 
-export default function Login() {
+const StyledContainer = styled(Container)({
+  marginTop: '100px',
+  backgroundColor: '#f4f6f8',
+  padding: '40px',
+  borderRadius: '10px',
+  paddingBottom: '100px',
+});
+
+const StyledButton = styled(Button)({
+  marginTop: '40px',
+});
+
+const StyledGrid = styled(Grid)({
+  marginTop: '40px',
+});
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [account, setAccount] = useState('');
+
+  useEffect(() => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      window.ethereum.enable();
+    }
+  }, []);
+
+  const handleSignIn = (event) => {
+    event.preventDefault();
+    console.log(`Username: ${username}, Password: ${password}`);
+    // Add authentication logic 
+  };
+
+  const handleSignInMetaMask = async () => {
+    const accounts = await window.web3.eth.getAccounts();
+    setAccount(accounts[0]);
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-      <TextField placeholder="Username" type="text" />
-      <br />
-      <TextField placeholder="Password" type="password" />
-      <br />
-      <FormControlLabel control={<Checkbox />} label="Remember me" />
-      <br />
-      <Button variant="contained" color="primary">Sign In</Button>
-      <br />
-      <Button color="primary">Forgot password?</Button>
-      <br />
-      <Button variant="contained" color="secondary">Sign in with MetaMask</Button>
-    </div>
+    <StyledContainer maxWidth="xs">
+      <Typography variant="h4" align="center" gutterBottom>
+        Login
+      </Typography>
+
+      <TextField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        id="username"
+        label="Username"
+        name="username"
+        autoComplete="username"
+        autoFocus
+        onChange={(event) => setUsername(event.target.value)}
+      />
+
+      <TextField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        name="password"
+        label="Password"
+        type="password"
+        id="password"
+        autoComplete="current-password"
+        onChange={(event) => setPassword(event.target.value)}
+      />
+
+      <FormControlLabel
+        control={<Checkbox value="remember" color="primary" />}
+        label="Remember me"
+      />
+
+      <StyledButton
+        type="submit"
+        fullWidth
+        variant="contained"
+        color="primary"
+        onClick={handleSignIn}
+      >
+        Sign In
+      </StyledButton>
+
+      <StyledGrid container>
+        <Grid item xs>
+          <Typography variant="body2">
+            Forgot password?
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="body2">
+            {"Don't have an account? Sign Up"}
+          </Typography>
+        </Grid>
+      </StyledGrid>
+
+      <StyledButton variant="contained" color="primary" fullWidth onClick={handleSignInMetaMask}>
+        Sign in with MetaMask
+      </StyledButton>
+
+    </StyledContainer>
   );
-}
+};
+
+export default Login;
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,17 +1,74 @@
 import React from 'react';
-import { Button, TextField } from '@mui/material';
+import { TextField, Button, Typography, Container, Grid } from '@mui/material';
+import { styled } from '@mui/system';
+
+const StyledContainer = styled(Container)({
+  marginTop: '100px',
+  backgroundColor: '#f4f6f8',
+  padding: '40px',
+  borderRadius: '10px',
+  paddingBottom: '100px'
+});
+
+const StyledButton = styled(Button)({
+  marginTop: '40px',
+});
+
+const StyledTextField = styled(TextField)({
+  marginTop: '20px',
+});
 
 export default function Signup() {
   return (
-    <div>
-      <h1>Sign Up</h1>
-      <TextField placeholder="Set username" type="text" />
-      <br />
-      <TextField placeholder="Set password" type="password" />
-      <br />
-      <TextField placeholder="Set recovery email" type="email" />
-      <br />
-      <Button variant="contained" color="primary">Sign Up</Button>
-    </div>
+    <StyledContainer maxWidth="xs">
+      <Typography variant="h4" align="center" gutterBottom>
+        Sign Up
+      </Typography>
+      
+      <StyledTextField 
+        variant="outlined" 
+        margin="normal" 
+        required 
+        fullWidth 
+        id="username" 
+        label="Username" 
+        name="username" 
+        autoComplete="username" 
+        autoFocus 
+      />
+
+      <StyledTextField 
+        variant="outlined" 
+        margin="normal" 
+        required 
+        fullWidth 
+        name="password" 
+        label="Password" 
+        type="password" 
+        id="password" 
+        autoComplete="current-password" 
+      />
+
+      <StyledTextField 
+        variant="outlined" 
+        margin="normal" 
+        required 
+        fullWidth 
+        name="email" 
+        label="Email" 
+        type="email" 
+        id="email" 
+        autoComplete="email" 
+      />
+
+      <StyledButton
+        type="submit"
+        fullWidth
+        variant="contained"
+        color="primary"
+      >
+        Sign Up
+      </StyledButton>
+    </StyledContainer>
   );
 }
